0f8f8fda187ad357b66348e8ba5cded8bcdc2aed
Diego Huaraca <diego.21.phs@hotmail.com>
2015-08-21 01:33:43 -0500

    update

--- script_ggvis.R
+++ script_ggvis.R
@@ -138,16 +138,7 @@ mtcars %>% group_by(cyl, am) %>% ggvis(~mpg, fill = ~interaction(cyl,am)) %>% la
 
 # 4) Interactivity and layers
 
-
-
-
-
-
-
-
-
-
-
+# Run this code and inspect the output. Follow the link in the instructions for the interactive version
 faithful %>% 
       ggvis(~waiting, ~eruptions, fillOpacity := 0.5, 
             shape := input_select(label = "Choose shape:", 
@@ -155,7 +146,20 @@ faithful %>%
                                               "triangle-up", "triangle-down"))) %>% 
       layer_points()
 
+# Copy the first code chunk and alter the code to make the fill property interactive using a select box
+faithful %>% 
+      ggvis(~waiting, ~eruptions, fillOpacity := 0.5, 
+            shape := input_select(label = "Choose shape:", 
+                                  choices = c("circle", "square", "cross", "diamond", 
+                                              "triangle-up", "triangle-down")),
+            fill:= input_select(label="Choose color:",
+                                choices = c("black", "red", "blue", "green"))) %>% 
+      layer_points()
 
+# Add radio buttons to control the fill of the plot
 mtcars %>% 
       ggvis(~mpg, ~wt, fill:= input_radiobuttons(label="Choose color", choices=c("black", "red", "blue", "green"))) %>% 
       layer_points()
+
+
+
